95bd4c5161a84ea50f7d1686ea252501b9376a88
friedooo <friedlich3@gmail.com>
2020-03-17 21:31:12 +0300

    feat: get a quote

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// ====================== Header 
+
 let navbar = document.getElementById('navId')
 
-console.log(navbar);
 
 navbar.addEventListener('click', (event) => 
 {
@@ -9,3 +10,54 @@ navbar.addEventListener('click', (event) =>
     event.target.classList.add('active');
 });
 
+// ====================== Get a quote
+
+
+let send = document.getElementById('submit');
+let message = document.getElementsByClassName('message')[0];
+let messageBlock = document.getElementsByClassName('message-block')[0];
+
+send.addEventListener('click', () => {
+
+    event.preventDefault();
+
+message.classList.remove('hidden');
+messageBlock.classList.remove('hidden');
+
+let info = {
+}
+
+let name = document.getElementById('name').value;
+if (/^\s*$/.test(name))
+info.name = '';
+else
+info.name = name;
+
+let email = document.getElementById('email').value;
+if (/^\s*$/.test(email))
+info.email = '';
+else
+info.email = email;
+
+let subject = document.getElementById('subject').value;
+if (/^\s*$/.test(subject))
+info.subject = 'Без темы';
+else
+info.subject = `Тема: ${subject}`;
+
+let description = document.getElementById('description').value;
+if (/^\s*$/.test(description))
+info.description = 'Без описания';
+else
+info.description = `Описание: ${description}` ;
+
+document.getElementById('info').innerHTML = `Письмо отправлено </br> ${info.subject} </br>  ${info.description}`;
+});
+
+
+let closeBtn = document.getElementById('close-Btn');
+
+closeBtn.addEventListener('click', () => {
+    message.classList.add('hidden');
+    messageBlock.classList.add('hidden');
+})
