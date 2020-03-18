e1e183cfb6063f18548d15c7f2c4855425c3661e
friedooo <friedlich3@gmail.com>
2020-03-18 23:20:13 +0300

    feat: add Portfolio switch  tabs

--- script.js
+++ script.js
@@ -60,4 +60,51 @@ let closeBtn = document.getElementById('close-Btn');
 closeBtn.addEventListener('click', () => {
     message.classList.add('hidden');
     messageBlock.classList.add('hidden');
-})
+});
+
+
+
+
+/* ==================================== Portfolio switch  tabs */
+
+
+let sortbar = document.getElementsByClassName('sortbar')[0];
+sortbar.addEventListener('click', (event) => 
+{
+
+    if (event.target.parentNode.className != 'sortbar__active' && event.target.className != 'sortbar__active')
+    {
+        // Переключение картинок на одну влево
+
+    let images = document.getElementsByClassName('portfolio__images')[0].children;
+[images[0].src, images[1].src, images[2].src, 
+images[3].src, images[4].src, images[5].src, 
+images[6].src, images[7].src, images[8].src,
+ images[9].src, images[10].src, images[11].src] 
+ = 
+[images[1].src, images[2].src, images[3].src, 
+images[4].src, images[5].src, images[6].src, 
+images[7].src, images[8].src, images[9].src, 
+images[10].src, images[11].src, images[0].src];
+    }
+
+    //делегирование передачи класса и удаление класса с остальных элементов
+
+    sortbar.querySelectorAll('li span').forEach(elem => {
+        elem.classList.remove('sortbar__active');
+    });
+    sortbar.querySelectorAll('li button').forEach(elem => {
+        elem.classList.remove('sortbar__active');
+    });
+    
+        if (event.target.parentNode.tagName == 'BUTTON')
+    event.target.parentNode.classList.add('sortbar__active');
+    else if (event.target.parentNode.tagName == 'LI')
+    event.target.classList.add('sortbar__active');
+
+   
+
+});
+
+
+
